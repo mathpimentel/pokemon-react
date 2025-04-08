99850cf93e9671adee42801641b950896fa34a74
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-07 22:37:03 -0300

    criando a instância do Axios e unindo todos os componentes no index

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+// src/components/index.ts
+export { default as PokemonCard } from "./PokemonCard";
+export { default as PokemonImage } from "./PokemonImage";
+export { default as PokemonTypes } from "./PokemonTypes";
+export { default as PokemonStats } from "./PokemonStats";
+export { default as StatBar } from "./StatBar";
+export { default as PokemonAbilities } from "./PokemonAbilities";
