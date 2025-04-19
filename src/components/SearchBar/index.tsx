0e63ec5fdc6b7a1b4caae573b853418e74081f63
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 01:05:03 -0300

    fix - refatorando SearchBar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,54 +1,27 @@
-import React from 'react';
+import React from "react";
+import styles from "./styles.module.css"
 
-// Tipagem dos props
 interface SearchBarProps {
   inputValue: string;
   onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onSearch: () => void;
 }
 
-const SearchBar= ({ inputValue, onInputChange, onSearch } : SearchBarProps) => {
+const SearchBar = ({ inputValue, onInputChange, onSearch }: SearchBarProps) => {
   return (
-    <div style={styles.container}>
+    <div className={styles.container}> 
       <input
         type="text"
         value={inputValue}
         onChange={onInputChange}
-        placeholder="Digite o nome ou o número da pokédex do pokémon"
-        style={styles.input}
+        className={styles.input}  
+        placeholder="Digite o nome ou o número da pokédex do pokémon..."
       />
-      <button onClick={onSearch} style={styles.button}>
+      <button onClick={onSearch} className={styles.button}> 
         Pesquisar
       </button>
     </div>
   );
-};
-
-// Estilos simples para o componente
-const styles = {
-  container: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: '20px',
-  },
-  input: {
-    padding: '10px',
-    fontSize: '16px',
-    marginRight: '10px',
-    border: '1px solid #ccc',
-    borderRadius: '4px',
-    width: '500px',
-  },
-  button: {
-    padding: '10px 20px',
-    fontSize: '16px',
-    cursor: 'pointer',
-    border: '1px solid #007BFF',
-    backgroundColor: '#007BFF',
-    color: 'white',
-    borderRadius: '4px',
-  }
 };
 
 export default SearchBar;
