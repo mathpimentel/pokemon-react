3ceb35f329ea8f8f5952afbe1f5c1d423ecdfe93
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 01:05:32 -0300

    feat - component ImageToogleButtons

---
+++ src/components/PokemonImageViewer/components/ImageToogleButtons/index.tsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+const index = () => {
+  return (
+    <div className="button-image-group">
+      <button>Normal</button>
+      <button>Brilhante</button>
+    </div>
+  );
+};
+
+export default index;
