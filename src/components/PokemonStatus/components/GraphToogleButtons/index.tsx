023eeae84eeb992ce7e65a14d657c445d50c6786
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 16:28:19 -0300

    feat - component GraphToogleButtons

---
+++ src/components/PokemonStatus/components/GraphToogleButtons/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css"
+const index = () => {
+  return (
+    <div className="button-image-group">
+      <button>Radar</button>
+      <button>Barra</button>
+    </div>
+  );
+};
+
+export default index;
