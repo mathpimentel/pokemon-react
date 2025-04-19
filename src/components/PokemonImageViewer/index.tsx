f486aaa36de535cdd0bbae80eb27dc35a877e1db
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 16:28:40 -0300

    fix - component PokemonImageViewer

--- src/components/PokemonImageViewer/index.tsx
+++ src/components/PokemonImageViewer/index.tsx
@@ -1,13 +1,30 @@
 import ImageToogleButtons from "./components/ImageToogleButtons";
 import PokemonImage from "./components/PokemonImage";
-
+import { PokemonSprites} from "../../types/pokemon";
+import { useState } from "react";
 import "./style.css";
-const index = () => {
+
+interface PokemonImageViewerProps {
+  sprites: PokemonSprites;
+}
+
+const index = ({ sprites }: PokemonImageViewerProps) => {
+  const [showShiny, setShowShiny] = useState(false);
+
+  const handleToggle = (value: boolean) => {
+    setShowShiny(value);
+  };
+
+  const imageSrc = showShiny
+  ? sprites.other["official-artwork"].front_shiny || sprites.other["official-artwork"].front_default
+  : sprites.other["official-artwork"].front_default;
+
+
   return (
     <>
       <section className="pokemon-photo">
-        <ImageToogleButtons />
-        <PokemonImage src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png" />
+        <ImageToogleButtons onToggle={handleToggle} showShiny={showShiny}/>
+        <PokemonImage src={imageSrc} />
       </section>
     </>
   );
