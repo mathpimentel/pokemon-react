7ca8191c3dc60a5378110feb8c1185d719a20070
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 01:08:11 -0300

    fix - exports components

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,10 +1,4 @@
-export { default as PokeCard } from "./PokeCard";
-export { default as PokeImage } from "./PokeImage";
-export { default as PokeTypes } from "./PokeTypes";
-export { default as PokeStats } from "./PokeStats";
-export { default as PokeAbilities } from "./PokeAbilities";
-export { default as PokeInfo } from "./PokeInfo";
-
-export { default as Loading } from "./Loading/index";
-export { default as StatusBar } from "./StatusBar";
-export { default as SearchBar } from "./SearchBar";
+export { default as Loading } from "./Loading/";
+export { default as SearchBar } from "./SearchBar/";
+export { default as PokemonInfo } from "./PokemonInfo/";
+export { default as PokemonImageViewer } from "./PokemonImageViewer";
