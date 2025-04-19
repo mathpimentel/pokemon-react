9424a965d4b714a0c7f0c8ad952edc628807e216
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-18 23:57:04 -0300

    feat - component PokemonSize

---
+++ src/components/PokemonInfo/components/PokemonSize/index.tsx
@@ -0,0 +1,16 @@
+const index = () => {
+  return (
+    <>
+      <tr>
+        <th>Altura</th>
+        <td>60 cm</td>
+      </tr>
+      <tr>
+        <th>Peso</th>
+        <td>13 kg</td>
+      </tr>
+    </>
+  );
+};
+
+export default index;
