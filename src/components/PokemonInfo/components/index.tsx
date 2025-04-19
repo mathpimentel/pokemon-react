985114785bad73d3d20b231188b13df7ec9a576a
Matheus Pimentel <mathcamposp@gmail.com>
2025-04-19 01:08:00 -0300

    fix - exports components

---
+++ src/components/PokemonInfo/components/index.tsx
@@ -0,0 +1,4 @@
+export { default as PokemonAbilities } from "./PokemonAbilities/";
+export { default as PokemonHeader } from "./PokemonHeader/";
+export { default as PokemonTypes } from "./PokemonTypes/";
+export { default as PokemonSize } from "./PokemonSize/";
